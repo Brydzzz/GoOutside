43869531468327c90c05b0dd23bbe011f5b34d15
Brydzzz <mrufkazkrem@protonmail.com>
2025-09-17 21:18:40 +0200

    prepared viewmodel outline

--- app/src/main/java/com/example/gooutside/ui/photo/PhotoModeViewModel.kt
+++ app/src/main/java/com/example/gooutside/ui/photo/PhotoModeViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.gooutside.ui.photo
 
+import android.util.Log
+import androidx.annotation.DrawableRes
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gooutside.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,25 +25,47 @@ class PhotoModeViewModel @Inject constructor() : ViewModel() {
         _uiState.update { it.copy(flashMode = it.flashMode.next()) }
     }
 
-    fun toggleCamera() {
+    fun toggleCameraFacing() {
         _uiState.update { it.copy(cameraFacing = it.cameraFacing.toggle()) }
     }
 
-    fun onTakePhotoClick() {
+    // TODO: implement onCapture, savePhoto and analyzeImage
+    fun onCapture() {
+        Log.d("PhotoModeViewModel", "onCapture")
+        analyzeImage()
     }
 
-    private fun analyzeImage() {}
+    fun savingToDiaryDismissed() {
+        _uiState.update { it.copy(analysisPassed = false) }
+    }
+
+    fun savePhoto() {
+        // save Photo to storage and add entry to diary
+        _uiState.update { it.copy(analysisPassed = false) }
+        Log.d("PhotoModeViewModel", "savePhoto")
+    }
+
+    private fun analyzeImage() {
+        viewModelScope.launch {
+            // simulation - temporary
+            _uiState.update { it.copy(isAnalysing = true) }
+            delay(1000) // simulate analysis
+            _uiState.update { it.copy(isAnalysing = false) }
+            _uiState.update { it.copy(analysisPassed = true) }
+        }
+        Log.d("PhotoModeViewModel", "analyzeImage")
+    }
 }
 
-enum class FlashMode(val value: Int) {
-    OFF(ImageCapture.FLASH_MODE_OFF),
-    ON(ImageCapture.FLASH_MODE_ON),
-    AUTO(ImageCapture.FLASH_MODE_AUTO);
+enum class FlashMode(val value: Int, @DrawableRes val icon: Int) {
+    OFF(ImageCapture.FLASH_MODE_OFF, R.drawable.ic_flash_off_24),
+    ON(ImageCapture.FLASH_MODE_ON, R.drawable.ic_flash_on_24),
+    AUTO(ImageCapture.FLASH_MODE_AUTO, R.drawable.ic_flash_auto_24);
 
     fun next(): FlashMode = when (this) {
-        OFF -> ON
-        ON -> AUTO
-        AUTO -> OFF
+        OFF -> AUTO
+        AUTO -> ON
+        ON -> OFF
     }
 }
 
@@ -54,5 +82,7 @@ enum class CameraFacing(val value: CameraSelector) {
 data class PhotoModeUiState(
     val cameraFacing: CameraFacing = CameraFacing.BACK,
     val flashMode: FlashMode = FlashMode.OFF,
+    val isAnalysing: Boolean = false,
+    val analysisPassed: Boolean = false
 )
 
